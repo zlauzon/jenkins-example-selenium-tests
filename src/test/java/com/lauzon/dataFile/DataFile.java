5aa0d06c968ea14fdebd00809f2200085cc8dfcd
zachary.lauzon <zachary.lauzon@fdmgroup.com>
2025-01-21 16:23:49 -0600

    create dataFile and define web app being tested

---
+++ src/test/java/com/lauzon/dataFile/DataFile.java
@@ -0,0 +1,6 @@
+//Data File contains universal variables for the web app being tested
+package com.lauzon.dataFile;
+
+public class DataFile {
+    public static String homeURL = "https://demoqa.com/";
+}
